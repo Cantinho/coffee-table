3dee4f17d3986c849a4c83deb647cb7d623d17f0
Samir Trajano Feitosa <samir.feitosa@virtus.ufcg.edu.br>
2019-10-08 23:02:31 -0300

    wip:add local database

---
+++ coffee_table/lib/src/data/repositories/dao/photos_table.dart
@@ -0,0 +1,11 @@
+//import 'package:moor_flutter/moor_flutter.dart';
+//
+//// assuming that your file is called photos_table.dart. This will give an error
+//// at first, but it's needed for moor to know about the generated code
+//part photos_table.g.dart;
+//
+//// this will generate a table called  "photos" for us. The rows of that table
+//// will be represented by a class called "Photo"
+//class Photos extends Table {
+//
+//}
